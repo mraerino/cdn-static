d49cf056d82651d2d9ae2145558c34a2e741b5f3
Marcus Weiner <marcus.weiner@gmail.com>
2018-11-23 22:03:37 +0100

    :construction: Provide wasabi-s3 example

--- cli/index.ts
+++ cli/index.ts
@@ -1,5 +1,19 @@
+import * as AWS from 'aws-sdk';
+
 const main = async () => {
     console.log("Hello World!");
+
+    // Create an S3 client
+    const s3 = new AWS.S3({endpoint: 's3.wasabisys.com'});
+
+    // Create a bucket and upload something into it
+    const bucketName = 'node-sdk-sample';
+    const keyName = 'hello_world.txt';
+
+    await s3.createBucket({Bucket: bucketName}).promise()
+
+    const params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
+    await s3.putObject(params).promise()
 }
 
 if (require.main === module) {
